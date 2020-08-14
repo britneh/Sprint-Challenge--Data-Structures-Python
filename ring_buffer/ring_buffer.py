c4633d4ed4d92bc42969ed1246588ce63fde3a33
britneh <brhacket@gmail.com>
2020-08-14 08:30:02 -0700

    first commit ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
 
-    def append(self, item):
-        pass
+    class __Full:
+        def append(self, item):
+            self.data[self.cur] = item
+            self.cur = (self.cur+1) % self.capacity
+
+        def get(self):
+            return self.data[self.cur:]+self.data[:self.cur]
+
+    def append(self,item):
+        """append an element at the end of the buffer"""
+        self.data.insert(item)
+        if len(self.data) == self.capacity:
+            self.cur = 0
+            # Permanently change self's class from non-full to full
+            self.__class__ = self.__Full
 
     def get(self):
-        pass
+        """ Return a list of elements from the oldest to the newest. """
+        return self.data
